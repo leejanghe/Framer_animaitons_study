a55b10c7457ce6c20c81f771d449cd247379d255
leejanghe <babybye777@gmail.com>
2023-01-12 23:21:21 +0900

    framer study

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,10 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+import Gesturesone from "./anicomponents/gestures_one";
+import Gesturestwo from "./anicomponents/gestures_two";
+import Motionvalues from "./anicomponents/motionvalues";
+import Variantsone from "./anicomponents/variants_one";
+import Variantstwo from "./anicomponents/variants_two";
 
 function App() {
-  return (
-    <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
-    </Wrapper>
-  );
+  return <Motionvalues />;
 }
 export default App;
---
+++ client/src/anicomponents/gestures_one.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: { scale: 1.5, rotatez: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
+};
+
+function Gesturesone() {
+  return (
+    <Wrapper>
+      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <Box
+        variants={boxVariants}
+        whileHover="hover"
+        whileTap="click"
+        drag
+        whileDrag="drag"
+      />
+    </Wrapper>
+  );
+}
+
+export default Gesturesone;
